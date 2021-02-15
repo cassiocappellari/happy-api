a4f131b276e3e2a2a1f825c2865cafdd1a9dc785
Cassio Cappellari <cassiocappellari@gmail.com>
2021-02-15 09:01:28 -0300

    feat: create image and orphanage views in order to manipulate data that will be sended to the frontend; update orphanage controller with views methods

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -1,21 +1,26 @@
 import {Request, Response} from 'express'
 import {getRepository} from 'typeorm'
 import Orphanage from '../models/Orphanage'
+import orphanageView from '../views/orphanages_view'
 
 export default {
     async index(req: Request, res: Response) {
         const orphanagesRepository = getRepository(Orphanage)
-        const orphanages = await orphanagesRepository.find()
+        const orphanages = await orphanagesRepository.find({
+            relations: ['images']
+        })
 
-        return res.json(orphanages)
+        return res.json(orphanageView.renderMany(orphanages))
     },
     async show(req: Request, res: Response) {
         const {id} = req.params
 
         const orphanagesRepository = getRepository(Orphanage)
-        const orphanage = await orphanagesRepository.findOneOrFail(id)
+        const orphanage = await orphanagesRepository.findOneOrFail(id, {
+            relations: ['images']
+        })
 
-        return res.json(orphanage)
+        return res.json(orphanageView.render(orphanage))
     },
     async create(req: Request, res: Response) {
         const {
