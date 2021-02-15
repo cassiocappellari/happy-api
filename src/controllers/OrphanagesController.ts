17876693741f74b0b3bc68a786605961469553c9
Cassio Cappellari <cassiocappellari@gmail.com>
2021-02-15 17:17:54 -0300

    feat: install yup lib for data validation; update create orphanages route with data validation before send to database using yup; update error handler with validation errors

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -2,6 +2,7 @@ import {Request, Response} from 'express'
 import {getRepository} from 'typeorm'
 import Orphanage from '../models/Orphanage'
 import orphanageView from '../views/orphanages_view'
+import * as yup from 'yup'
 
 export default {
     async index(req: Request, res: Response) {
@@ -40,8 +41,8 @@ export default {
         const images = requestImages.map(image => {
             return {path: image.filename}
         })
-    
-        const orphanage = orphanagesRepository.create({
+
+        const data = {
             name,
             latitude,
             longitude,
@@ -50,8 +51,28 @@ export default {
             opening_hours,
             open_on_weekends,
             images
+        }
+
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            latitude: yup.number().required(),
+            longitude: yup.number().required(),
+            about: yup.string().required().max(300),
+            instructions: yup.string().required(),
+            opening_hours: yup.string().required(),
+            open_on_weekends: yup.boolean().required(),
+            images: yup.array(
+                yup.object().shape({
+                    path: yup.string().required()
+                })
+            )
+        })
+
+        await schema.validate(data, {
+            abortEarly: false
         })
     
+        const orphanage = orphanagesRepository.create(data)
         await orphanagesRepository.save(orphanage)
     
         return res.status(201).json(orphanage)
