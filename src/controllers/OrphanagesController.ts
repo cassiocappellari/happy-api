53b9ac12cd99fb4c467f787c4c55ee1eb5740741
Cassio Cappellari <cassiocappellari@gmail.com>
2021-02-15 07:54:29 -0300

    feat: create image model; create relation between images and orphanages tables; update orphanage controller with feature of send images path to the database

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -29,6 +29,12 @@ export default {
         } = req.body
     
         const orphanagesRepository = getRepository(Orphanage)
+
+        const requestImages = req.files as Express.Multer.File[]
+
+        const images = requestImages.map(image => {
+            return {path: image.filename}
+        })
     
         const orphanage = orphanagesRepository.create({
             name,
@@ -37,7 +43,8 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends
+            open_on_weekends,
+            images
         })
     
         await orphanagesRepository.save(orphanage)
